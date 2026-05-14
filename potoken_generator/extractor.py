2464541ea9dff9742a2648529a18219ea53a9383
manuelalv <67434946+manuelalv@users.noreply.github.com>
2026-05-14 11:42:38 -0600

    Refactor browser start parameters for nodriver

--- potoken_generator/extractor.py
+++ potoken_generator/extractor.py
@@ -101,10 +101,17 @@ class PotokenExtractor:
             logger.info('update started')
             self._extraction_done.clear()
             try:
-                browser = await nodriver.start(headless=False,
+                browser = await nodriver.start(
+                               headless=False,
                                no_sandbox=True,
-                                               browser_executable_path=self.browser_path,
-                                               user_data_dir=self.profile_path)
+                               browser_args=[
+                                   '--no-sandbox',
+                                   '--disable-setuid-sandbox',
+                                   '--disable-dev-shm-usage',
+                                   '--disable-gpu',
+                               ],
+                               browser_executable_path=self.browser_path,
+                               user_data_dir=self.profile_path)
             except FileNotFoundError as e:
                 msg = "could not find Chromium. Make sure it's installed or provide direct path to the executable"
                 raise FileNotFoundError(msg) from e
